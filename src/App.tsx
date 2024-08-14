bba5380bc0ebdb242b75a1b738ce9d86d9205380
Laquita Childress <LChildress@tql.com>
2024-08-14 11:02:17 -0400

    first init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <>Ready</>;
+}
+
+export default App;
